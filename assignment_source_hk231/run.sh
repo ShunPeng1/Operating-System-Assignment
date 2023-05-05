bce0e920e578b232ed647f3178d496a85d5fd0c4
Quessalini03 <vuchauduyquang@gmail.com>
2023-05-05 13:18:49 +0700

    tests

--- assignment_source_hk231/run.sh
+++ assignment_source_hk231/run.sh
@@ -1,6 +1,6 @@
 make clean
 make all
 # ./os os_1_singleCPU_mlq
-./os os_1_singleCPU_mlq_paging
-./os os_1_singleCPU_mlq_paging
+./os os_0_mlq_paging
+# ./os os_1_singleCPU_mlq_paging
 # ./os custom
