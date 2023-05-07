50569f626ac3baf7d82a4687aacfaef30067aa04
ShunPeng1 <shunpeng1@gmail.com>
2023-05-07 22:21:40 +0700

    refactor: os-cfg remove comment

--- assignment_source_hk231/include/os-cfg.h
+++ assignment_source_hk231/include/os-cfg.h
@@ -5,13 +5,11 @@
 #define MAX_PRIO 140
 
 #define MM_PAGING
-/* Thuan Approved configable */
 
 // #define MM_FIXED_MEMSZ
-#define VMDBG 1 // Quang changed this
+#define VMDBG 1 
 #define PAGING_DBG 1
-// #define MMDBG 1
-/* End of Thuan Approved */
+#define MMDBG 1
 
 #define IODUMP 1
 #define PAGETBL_DUMP 1
